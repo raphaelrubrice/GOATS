0f9296e56c24783a5c817945395b62ff3315eae4
Aelrach <raphaelsup@outlook.com>
2024-11-19 18:09:17 +0100

    Fixed functionalities, added possibility for user usage from command line

--- goats.py
+++ goats.py
@@ -1,61 +1,63 @@
 import mygene as mg
+import numpy as np
+import gzip
+from copy import deepcopy
+from pronto import Ontology
 
-mg = mg.MyGeneInfo()
+def add_item(liste, item):
+    if item not in liste:
+        liste.append(item)
+        return True
+    return False
 
-def get_term_ids(go_list, numeric=False):
-    if numeric:
-        return [int(term['id'][3:]) for term in go_list]
-    return [term['id'] for term in go_list]
+def parse_gaf(file_path, geneset, ontology):
+    gene_dico = {'ids':{'MF':[], 'BP':[], 'CC':[]},
+                    'str_ids':{'MF':[], 'BP':[], 'CC':[]},
+                    'terms':{'MF':[], 'BP':[], 'CC':[]},
+                    'predicate':{'MF':[], 'BP':[], 'CC':[]},
+                    'all_ids':[],
+                    'all_collections':[]}
+    info_dico = {gene: deepcopy(gene_dico) for gene in geneset}
+    found_flags = {gene:False for gene in geneset}
 
-def get_term_descriptions(go_list):
-    return [term['term'] for term in go_list]
+    translate_collection = {'P':'BP', 'F':'MF', 'C':'CC'}
+    description_memory = {}
+    with gzip.open(file_path, 'rt') as f:  # Use 'open' instead if file is not gzipped
+        for line in f:
+            if line.startswith("!"):  # Skip header lines
+                continue
+            cols = line.strip().split('\t')
+            all_names = cols[10].split('|') if not (cols[10] == "") else None
+            if all_names == None:
+                all_names = set(cols[2]) if not (cols[2] == "") else None
+            if all_names != None:
+                predicate = cols[3]
+                go_term = cols[4]  
+                go_number = int(go_term[3:])
+                if go_term not in description_memory.keys():
+                    go_description = ontology.get(go_term).name
+                    description_memory[go_term] = go_description
+                else:
+                    go_description = description_memory[go_term]
+                collection = translate_collection[cols[8]] # MF, BP or CC
+                for gene in geneset:
+                    gene_dico = info_dico[gene]
+                    if gene in all_names:
+                        flag_int_id = add_item(gene_dico['ids'][collection], go_number)
+                        flag_str_id = add_item(gene_dico['str_ids'][collection], go_term)
 
-def get_gene_predicate(go_list):
-    return [term['qualifier'] for term in go_list]
-
-def get_infos(geneset):
-    info_dico = {gene:{} for gene in geneset}
-    out = mg.querymany(geneset, scopes='symbol,entrezgene,ensemblgene', species=9606, fields=['entrezgene','go'])
-    for i in range(len(out)):
-        if 'notfound' in out[i].keys():
-            gene = out[i]['query']
-            print(f'\n{gene} not found. Removing {gene}.')
-            geneset.remove(gene)
-    for i in range(len(geneset)):
-        goterms = out[i]['go']
-        go_mf = goterms['MF'] if 'MF' in goterms.keys() else None
-        mf_go_ids = get_term_ids(go_mf, numeric=True)
-        mf_go_desc = get_term_descriptions(go_mf)
-        mf_go_pred = get_gene_predicate(go_mf)
-
-        go_bp = goterms['BP'] if 'BP' in goterms.keys() else None
-        bp_go_ids = get_term_ids(go_bp, numeric=True)
-        bp_go_desc = get_term_descriptions(go_bp)
-        bp_go_pred = get_gene_predicate(go_bp)
-
-        go_cc = goterms['CC'] if 'CC' in goterms.keys() else None
-        cc_go_ids = get_term_ids(go_cc, numeric=True)
-        cc_go_desc = get_term_descriptions(go_cc)
-        cc_go_pred = get_gene_predicate(go_cc)
-
-        # print("\nMF GO terms:", mf_go_desc)
-        # print("\nBP GO terms:", bp_go_desc)
-        # print("\nCC GO terms:", cc_go_desc)
-
-        # print("\nMF GO ids:", mf_go_ids)
-        # print("\nBP GO ids:", bp_go_ids)
-        # print("\nCC GO ids:", cc_go_ids)
-
-        gene_dico = {'ids':{'MF':mf_go_ids, 'BP':bp_go_ids, 'CC':cc_go_ids},
-                     'terms':{'MF':mf_go_desc, 'BP':bp_go_desc, 'CC':cc_go_desc},
-                     'predicate':{'MF':mf_go_pred, 'BP':bp_go_pred, 'CC':cc_go_pred}}
-        info_dico[geneset[i]].update(gene_dico)
-    return info_dico
-
-# def common_ids(infos):
-#     dico = {'MF':[], 'BP':[], 'CC':[]}
-#     for section in ['MF', 'BP', 'CC']:
-#         infos[gene]['ids'][section]
+                        if flag_int_id and flag_str_id:
+                            gene_dico['terms'][collection].append(go_description)
+                            gene_dico['predicate'][collection].append(predicate)
+                        found_flags[gene] = True
+    for gene in geneset:
+        if not found_flags[gene]:
+            print(f'\nGene {gene} not found. Removing {gene}..')
+            del info_dico[gene]
+        else:
+            info_dico[gene]['all_ids'] = info_dico[gene]['ids']['MF'] + info_dico[gene]['ids']['BP'] + info_dico[gene]['ids']['CC']
+            info_dico[gene]['all_collections'] = ['MF']*len(info_dico[gene]['ids']['MF']) + ['BP']*len(info_dico[gene]['ids']['BP']) + ["CC"]*len(info_dico[gene]['ids']['CC'])
+    return info_dico, list(info_dico.keys()), description_memory
 
 def show_relations(infos, gene, format='str', query='predicate', show=True):
     out = {gene:[]} if format=='str' else {}
@@ -82,81 +84,218 @@ def show_relations(infos, gene, format='str', query='predicate', show=True):
                     print(f'\n{go_term} : {go_relation}')
     return out
 
-def common_sets(geneset, collection=None):
-    assert (collection == None) or ('list' in str(type(collection))), "Collection must be None or a List object."
-    infos = get_infos(geneset)
-    go_collections = infos[geneset[0]]['ids'].keys()
+def find_top_terms(infos, geneset, threshold):
+    N_geneset = len(list(geneset))
+    assert N_geneset > 0, "Empty geneset."
 
-    if not collection:
-        collection = go_collections
-    in_common = {}
-    for c in collection:
-        common_goterms = set([])
-        gene_roles = {}
+    min_hit = int(np.floor(threshold * N_geneset))
+    print(len([go_id for gene in infos.keys() for go_id in infos[gene]['all_ids']]))
+    all_terms_geneset = set([go_id for gene in infos.keys() for go_id in infos[gene]['all_ids']])
+    print(len(all_terms_geneset))
+    kept = {}
+    for GO in all_terms_geneset:
+        count = 0
         for gene in infos.keys():
-            goterms = set(infos[gene]['ids'][c]) # list of all go term IDS for this gene for this collection
-            predicates = show_relations(infos, gene, format='int', query='predicate', show=False)
+            if GO in infos[gene]['all_ids']:
+                count += 1
+        if count >= min_hit:
+            kept[GO] = count / N_geneset
+    print(len(kept.keys()))
+    return kept
 
-            if len(common_goterms) == 0:
-                # On initialization
-                common_goterms = goterms
-            else:
-                # we update the set to be only IDS which are in common (INTERSECT)
-                common_goterms = common_goterms.intersection(goterms)
-
+def show_description(infos, go_id_list, show=True):
+    out = {}
+    for go_id in go_id_list:
+        count = 0
+        description_memory = []
+        tuple_list = []
         for gene in infos.keys():
-            gene_roles[gene] = {go_id:predicate for (go_id,predicate) in predicates.items() if go_id in common_goterms}
-        descriptions = show_relations(infos, geneset[0], format='int', query='terms', show=False)
-        
-        term_descriptions = [descriptions[go_id] for go_id in common_goterms]
-        in_common[c] = {'ids':common_goterms, 'predicates':gene_roles, 'descriptions':term_descriptions}
-    return in_common
+            if go_id in infos[gene]['all_ids']:
+                count += 1
+                go_id_index = infos[gene]['all_ids'].index(go_id)
+                collection = infos[gene]['all_collections'][go_id_index]
+                collection_go_index = infos[gene]['ids'][collection].index(go_id)
+                gene_predicate = infos[gene]['predicate'][collection][collection_go_index]
+                if count == 1:
+                    go_description = infos[gene]['terms'][collection][collection_go_index]
+                    description_memory.append(go_description)
+                    if show:
+                        print(f'\n{go_id} : {go_description}')
+                if show:
+                    print(f'\n{gene} -> {gene_predicate} -> {description_memory[0]}')
+                tuple_list.append((gene, gene_predicate, description_memory[0]))
+        out[go_id] = tuple_list
+    return out
 
-def get_role(in_common, collection, gene, go_id):
-    return in_common[collection]['predicates'][gene][go_id]
+def load_config(config_file):
+    """
+    Loads configuration settings from a file into a dictionary.
 
-def show_description(in_common, go_id_list=None):
-    if go_id_list:
-        for go_id in go_id_list:
-            for collection in in_common.keys():
-                if go_id in in_common[collection]['ids']:
-                    i = 0
-                    for item in in_common[collection]['ids']:
-                        if item == go_id:
-                            break
-                        i += 1
-                    desc = in_common[collection]['descriptions'][i]
-                    print(f"\n'{go_id}' : {desc}")
-                    for gene in in_common[collection]['predicates'].keys():
-                        print(f"{gene} -> {get_role(in_common, collection, gene, go_id)} -> {desc}")
-                    break
-    else:
-        memory = set([])
-        for collection in in_common.keys():
-            go_id_list = set(list(in_common[collection]['ids']))
-            for go_id in go_id_list:
-                if go_id in in_common[collection]['ids'] and go_id not in memory:
-                    i = 0
-                    for item in in_common[collection]['ids']:
-                        if item == go_id:
-                            break
-                        i += 1
-                    desc = in_common[collection]['descriptions'][i]
-                    print(f"\n'{go_id}' : {desc}")
-                    for gene in in_common[collection]['predicates'].keys():
-                        print(f"{gene} -> {get_role(in_common, collection, gene, go_id)} -> {desc}")
-                    memory.add(go_id)
-                    
+    Parameters:
+    config_file: str
+        Path to the configuration file.
 
-geneset = ['IL6', 'IL2', 'IFN', 'bonjour']
+    Returns:
+    dict
+        A dictionary with configuration key-value pairs.
+    """
+    config = {}
+    try:
+        with open(config_file, 'r') as file:
+            for line in file:
+                # Ignore comments and empty lines
+                if line.startswith('#') or not line.strip():
+                    continue
+                # Split the line into key and value
+                key, value = line.strip().split('=', 1)
+                config[key.strip()] = value.strip()
+    except FileNotFoundError:
+        print(f"Error: Configuration file {config_file} not found.")
+        exit(1)
+    except Exception as e:
+        print(f"Error while reading the configuration file: {e}")
+        exit(1)
+    
+    return config
 
-infos = get_infos(geneset)
-#print(infos)
-out = show_relations(infos, geneset[0], format='int', query='terms', show=False)
-#print(out)
-in_common = common_sets(geneset)
-print(in_common)
-print('\n\n')
-show_description(in_common)
+def create_info_dico_string(info_dico, genes):
+    """
+    Create a string representation of the `info_dico` dictionary for saving to a text file.
 
+    Args:
+        info_dico (dict): Dictionary containing parsed GAF data for each gene.
 
+    Returns:
+        str: A formatted string representation of `info_dico`.
+    """
+    output_lines = []
+    for gene, data in info_dico.items():
+        if gene in genes:
+            print(gene)
+            output_lines.append(f"Gene: {gene}\n")
+            output_lines.append("  Molecular Function (MF):\n")
+            for idx, go_id in enumerate(data['ids']['MF']):
+                term = data['terms']['MF'][idx]
+                predicate = data['predicate']['MF'][idx]
+                output_lines.append(f"    GO ID: {go_id} | Term: {term} | Predicate: {predicate}\n")
+
+            output_lines.append("  Biological Process (BP):\n")
+            for idx, go_id in enumerate(data['ids']['BP']):
+                term = data['terms']['BP'][idx]
+                predicate = data['predicate']['BP'][idx]
+                output_lines.append(f"    GO ID: {go_id} | Term: {term} | Predicate: {predicate}\n")
+
+            output_lines.append("  Cellular Component (CC):\n")
+            for idx, go_id in enumerate(data['ids']['CC']):
+                term = data['terms']['CC'][idx]
+                predicate = data['predicate']['CC'][idx]
+                output_lines.append(f"    GO ID: {go_id} | Term: {term} | Predicate: {predicate}\n")
+
+            # Add summary information
+            output_lines.append("  All GO IDs:\n")
+            output_lines.append(f"    {', '.join(map(str, data['all_ids']))}\n")
+            output_lines.append("  All Collections:\n")
+            output_lines.append(f"    {', '.join(data['all_collections'])}\n")
+            output_lines.append("\n")
+
+    # Join all lines into a single string
+    return "".join(output_lines)
+
+def create_top_terms_string(top_terms):
+    """
+    Create a string representation of the `top_terms` dictionary for saving to a text file.
+
+    Args:
+        top_terms (dict): Dictionary mapping GO terms to their relative frequency in a gene set.
+
+    Returns:
+        str: A formatted string representation of `top_terms`.
+    """
+    output_lines = []
+    output_lines.append("Top GO Terms by Frequency in Gene Set:\n")
+    output_lines.append("=======================================\n")
+    
+    # Format each term and its frequency
+    for go_term, frequency in sorted(top_terms.items(), key=lambda x: -x[1]):  # Sort by frequency, descending
+        output_lines.append(f"GO Term: GO:{str(go_term).zfill(7)} | Frequency: {frequency:.4f}\n")
+    
+    # Join all lines into a single string
+    return "".join(output_lines)
+
+def create_description_string(descriptions):
+    """
+    Create a string representation of the `descriptions` dictionary for saving to a text file.
+
+    Args:
+        descriptions (dict): Dictionary mapping GO terms to lists of tuples with gene, predicate, and description.
+
+    Returns:
+        str: A formatted string representation of `descriptions`.
+    """
+    output_lines = []
+    output_lines.append("GO Term Descriptions and Gene Relationships:\n")
+    output_lines.append("===========================================\n")
+
+    for go_id, relationships in descriptions.items():
+        output_lines.append(f"\nGO Term: GO:{str(go_id).zfill(7)}\n")
+        output_lines.append("-------------------------------------------\n")
+
+        if not relationships:
+            output_lines.append("No associated genes found.\n")
+        else:
+            for gene, predicate, description in relationships:
+                output_lines.append(f"Gene: {gene} | Predicate: {predicate} | Description: {description}\n")
+    
+    # Join all lines into a single string
+    return "".join(output_lines)
+
+def run_goats(config):
+    path_geneset = config["GENESET"]
+    geneset = []
+    with open(path_geneset, 'r') as f:
+        for line in f:
+            gene_symbol = line.strip()
+            if gene_symbol and 'str' in str(type(gene_symbol)):  # Only append if it's not an empty line
+                geneset.append(gene_symbol)
+
+    obo_file = config["OBO_FILE"]
+    gaf_file = config["GAF_FILE"]
+    threshold = float(config["THRESHOLD"])
+    print(threshold)
+    show_desc = True if int(config["SHOW_DESC"]) == 1 else False
+    save = True if int(config["SAVE"]) == 1 else False
+    timestamp = config["TIMESTAMP"]
+
+    ontology = Ontology(obo_file)
+    infos, genes_in_info, all_descriptions = parse_gaf(gaf_file, geneset, ontology)
+    if save:
+        filename = f"infos_goats_query_{timestamp}.txt"
+        txt = create_info_dico_string(infos, genes_in_info)
+        with open(filename, 'w') as f:
+            f.write(txt)
+
+    hits = find_top_terms(infos, genes_in_info, threshold)
+    if save:
+        filename = f"hits_goats_query_{timestamp}.txt"
+        txt = create_top_terms_string(hits)
+        with open(filename, 'w') as f:
+            f.write(txt)
+
+    out = show_description(infos, hits.keys(), show=show_desc)
+    if save:
+        filename = f"desc_goats_query_{timestamp}.txt"
+        txt = create_description_string(out)
+        with open(filename, 'w') as f:
+            f.write(txt)
+    return infos, hits, out
+
+if __name__ == "__main__":
+    #Load the configuration file path from command-line arguments
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python goats.py <config_file>")
+        exit(1)
+
+    config_file = sys.argv[1]
+    config = load_config(config_file)
+    infos, hits, out = run_goats(config)
---
+++ run.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+script_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+
+show_help() {
+    echo "Usage: $(basename "$0") [OPTIONS]"
+    echo ""
+    echo "Options:"
+    echo "  -g, --geneset          Path to geneset file. ONE GENE SYMBOL PER LINE. (required)"
+    echo "  -obo, --obo            Path to the .obo ontology file (required)"
+    echo "  -gaf, --gaf            Path to the .gaf annotation file (required)"
+    echo "  -threshold, --threshold"
+    echo "                         Threshold value for filtering (default: 0.5)"
+    echo "  -sd, --show_descriptions"
+    echo "                         Show descriptions for GO terms (default: 0)"
+    echo "  -save, --save          Save the outputs to a file (default: 1)"
+    echo "  -h, --help             Show this help message and exit"
+    echo ""
+    echo "Examples:"
+    echo "  $(basename "$0") -g geneset.txt -obo go-basic.obo -gaf goa_human.gaf -threshold 0.7"
+    echo "  $(basename "$0") --geneset geneset.txt --obo go-basic.obo --gaf goa_human.gaf --show_descriptions 1"
+    echo ""
+    echo "Note: Ensure that all required files are provided. Missing arguments will result in an error."
+    exit 1
+}
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --geneset|-g) geneset="$2"; shift ;;
+        --obo|-obo) obo_file="$2"; shift ;;
+        --gaf|-gaf) gaf_file="$2"; shift ;;
+        --threshold|-threshold) threshold="$2"; shift ;;
+        --show_descriptions|-sd) show_desc="$2"; shift ;;
+        --save|-save) save="$2"; shift ;;
+        --help|-h) show_help ;;
+        *) echo "Unknown parameter passed" $1; show_help ;;
+    esac
+    shift
+done
+
+geneset="${geneset:-0}"
+obo_file="${obo_file:-0}"
+gaf_file="${gaf_file:-0}"
+threshold="${threshold:-0.5}"
+show_desc="${show_desc:-0}"
+save="${save:-1}"
+
+if [[ -z $obo_file ]] || [[ -z $gaf_file ]] || [[ -z $geneset ]]; then
+    echo "Error, you either did not specify the path to the .OBO or .GAF files; or you did not specify any gene"
+    show_help
+fi
+
+current_date=$(date '+%Y-%m-%d') # Format: YYYY-MM-DD
+current_time=$(date '+%H:%M:%S') # Format: HH:MM:SS
+log_file="$script_dir/log_${current_date}_${current_time}.txt"
+cat > "$log_file" <<EOL
+GENESET=$geneset
+OBO_FILE=$obo_file
+GAF_FILE=$gaf_file
+THRESHOLD=$threshold
+SHOW_DESC=$show_desc
+SAVE=$save
+TIMESTAMP="${current_date}_${current_time}"
+EOL
+
+config_file="$script_dir/config.txt"
+cat > "$config_file" <<EOL
+GENESET=$geneset
+OBO_FILE=$obo_file
+GAF_FILE=$gaf_file
+THRESHOLD=$threshold
+SHOW_DESC=$show_desc
+SAVE=$save
+TIMESTAMP="${current_date}_${current_time}"
+EOL
+
+python=$(command -v python)
+$python "$script_dir/goats.py" "$config_file"
